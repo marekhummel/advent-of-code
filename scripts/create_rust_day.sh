ee2ca6bd27622610620c63ff51fd6cb3fc8af744
Marek Hummel <marek.hummel3@gmail.com>
2023-12-18 17:03:25 +0100

    Updated create script

--- scripts/create_rust_day.sh
+++ scripts/create_rust_day.sh
@@ -2,3 +2,11 @@ year=$1
 day=$(printf "%02d" $2)
 cp scripts/day.rs $year/rust/solutions/day$day.rs
 sed -i s:XX:$day: $year/rust/solutions/day$day.rs
+
+sed -i "$(($2+2)) a\use solutions::day$day;" $year/rust/main.rs 
+sed -i "$(($2*2+12)) a\        Box::new(day$day::Solution$day {})," $year/rust/main.rs 
+sed -i "$(($2-1)) a\pub mod day$day;" $year/rust/solutions/mod.rs
+
+touch $year/inputs/sample$day.txt
+touch $year/inputs/input$day.txt
+
