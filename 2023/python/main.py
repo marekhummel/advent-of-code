edca86370147e970d582b8d4c17490a2bfb957d1
Marek Hummel <marek.hummel3@gmail.com>
2023-12-26 13:01:50 +0100

    Stripped empty python soultions

--- 2023/python/main.py
+++ 2023/python/main.py
@@ -9,24 +9,7 @@ from solutions.day03 import Solution03
 from solutions.day04 import Solution04
 from solutions.day05 import Solution05
 from solutions.day06 import Solution06
-from solutions.day07 import Solution07
-from solutions.day08 import Solution08
-from solutions.day09 import Solution09
-from solutions.day10 import Solution10
-from solutions.day11 import Solution11
 from solutions.day12 import Solution12
-from solutions.day13 import Solution13
-from solutions.day14 import Solution14
-from solutions.day15 import Solution15
-from solutions.day16 import Solution16
-from solutions.day17 import Solution17
-from solutions.day18 import Solution18
-from solutions.day19 import Solution19
-from solutions.day20 import Solution20
-from solutions.day21 import Solution21
-from solutions.day22 import Solution22
-from solutions.day23 import Solution23
-from solutions.day24 import Solution24
 
 ALL: bool = True
 VERSION: Literal[1, 2] = 1
@@ -34,37 +17,20 @@ USE_SAMPLE: bool = True
 
 
 if __name__ == "__main__":
-    solutions = [
-        Solution01,
-        Solution02,
-        Solution03,
-        Solution04,
-        Solution05,
-        Solution06,
-        Solution07,
-        Solution08,
-        Solution09,
-        Solution10,
-        Solution11,
-        Solution12,
-        Solution13,
-        Solution14,
-        Solution15,
-        Solution16,
-        Solution17,
-        Solution18,
-        Solution19,
-        Solution20,
-        Solution21,
-        Solution22,
-        Solution23,
-        Solution24,
-    ]
+    solutions = {
+        1: Solution01,
+        2: Solution02,
+        3: Solution03,
+        4: Solution04,
+        5: Solution05,
+        6: Solution06,
+        12: Solution12,
+    }
 
     arg = sys.argv[1]
 
     if arg == "main":
-        for sc in solutions:
+        for sc in solutions.values():
             s = sc()
             print(f"Day {s.day:02d}:")
             for version in [1, 2]:
