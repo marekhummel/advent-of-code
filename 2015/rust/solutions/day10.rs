7feac611380f8b8fc497b39c37bdac54b9881c4f
Marek Hummel <marek.hummel3@gmail.com>
2023-12-31 14:55:09 +0100

    Solved #10 in 2015

---
+++ 2015/rust/solutions/day10.rs
@@ -0,0 +1,31 @@
+use aoc_lib::solution::Solution;
+use aoc_lib::types::{IntoSome, ProblemInput, ProblemResult};
+use itertools::Itertools;
+
+pub struct Solution10;
+impl Solution10 {
+    fn look_and_say(value: &str) -> String {
+        value
+            .chars()
+            .group_by(|c| *c)
+            .into_iter()
+            .map(|(v, g)| format!("{}{v}", g.count()))
+            .join("")
+    }
+}
+
+impl Solution for Solution10 {
+    fn solve_version01(&self, input: ProblemInput, _is_sample: bool) -> Option<ProblemResult> {
+        let iterations = if _is_sample { 5 } else { 40 };
+        let final_str = (0..iterations).fold(input.string(), |value, _| Self::look_and_say(&value));
+        final_str.len().into_some()
+    }
+
+    fn solve_version02(&self, input: ProblemInput, _is_sample: bool) -> Option<ProblemResult> {
+        // Computation by now takes some seconds, could use
+        // https://mathworld.wolfram.com/CosmologicalTheorem.html and fast exponentiation on matrices to solve
+        let iterations = if _is_sample { 5 } else { 50 };
+        let final_str = (0..iterations).fold(input.string(), |value, _| Self::look_and_say(&value));
+        final_str.len().into_some()
+    }
+}
