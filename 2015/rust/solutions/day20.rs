76f4b50a740cf062b5907540953dbd3218d1915c
Marek Hummel <marek.hummel3@gmail.com>
2024-01-03 00:44:17 +0100

    Solved #20 in 2015

---
+++ 2015/rust/solutions/day20.rs
@@ -0,0 +1,38 @@
+use aoc_lib::solution::Solution;
+use aoc_lib::types::{IntoSome, ProblemInput, ProblemResult};
+
+pub struct Solution20;
+impl Solution20 {
+    fn divsum(n: u64, lazy: bool) -> u64 {
+        if n == 0 || n == 1 {
+            return n;
+        }
+
+        let root_n = (n as f32).sqrt() as u64;
+        let sum: u64 = (1..=root_n)
+            .filter(|d| n % d == 0)
+            .flat_map(|d| if d * d == n { vec![d] } else { vec![d, n / d] })
+            .filter(|div| !lazy || div * 50 >= n)
+            .sum();
+
+        sum
+    }
+}
+
+impl Solution for Solution20 {
+    fn solve_version01(&self, input: ProblemInput, _is_sample: bool) -> Option<ProblemResult> {
+        let target = input.string().parse::<u64>().unwrap();
+        (1..)
+            .find(|house| Self::divsum(*house, false) * 10 >= target)
+            .unwrap()
+            .into_some()
+    }
+
+    fn solve_version02(&self, input: ProblemInput, _is_sample: bool) -> Option<ProblemResult> {
+        let target = input.string().parse::<u64>().unwrap();
+        (1..)
+            .find(|house| Self::divsum(*house, true) * 11 >= target)
+            .unwrap()
+            .into_some()
+    }
+}
