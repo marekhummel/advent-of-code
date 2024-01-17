fcbe4de02a19ef31fdd755c5392ddb06c1ab9bb4
Marek Hummel <marek.hummel3@gmail.com>
2024-01-17 17:09:54 +0100

    Solved #08 in 2018

--- 2018/rust/main.rs
+++ 2018/rust/main.rs
@@ -8,12 +8,13 @@ use solutions::day04;
 use solutions::day05;
 use solutions::day06;
 use solutions::day07;
+use solutions::day08;
 use std::env;
 
 use aoc_lib::solution::Solution;
 
 const ALL: bool = true;
-const VERSION: u8 = 2;
+const VERSION: u8 = 1;
 const USE_SAMPLE: bool = true;
 
 fn main() {
@@ -25,6 +26,7 @@ fn main() {
         Box::new(day05::Solution05 {}),
         Box::new(day06::Solution06 {}),
         Box::new(day07::Solution07 {}),
+        Box::new(day08::Solution08 {}),
         //
     ];
 
---
+++ 2018/rust/solutions/day08.rs
@@ -0,0 +1,57 @@
+use aoc_lib::iterator::ParsedExt;
+use aoc_lib::solution::Solution;
+use aoc_lib::types::{IntoSome, ProblemInput, ProblemResult};
+
+#[derive(Debug)]
+struct Node {
+    children: Vec<Node>,
+    metadata: Vec<u32>,
+}
+
+impl Node {
+    fn from_licence<I: Iterator<Item = u32>>(iter: &mut I) -> Self {
+        let num_children = iter.next().unwrap();
+        let num_metadata = iter.next().unwrap();
+
+        let children = (0..num_children).map(|_| Self::from_licence(iter)).collect();
+        let metadata = iter.take(num_metadata as usize).collect();
+        Node { children, metadata }
+    }
+
+    fn total_metadata(&self) -> u32 {
+        self.metadata.iter().sum::<u32>() + self.children.iter().map(|c| c.total_metadata()).sum::<u32>()
+    }
+
+    fn value(&self) -> u32 {
+        if self.children.is_empty() {
+            self.metadata.iter().sum::<u32>()
+        } else {
+            self.metadata
+                .iter()
+                .filter(|i| 1 <= **i && **i <= self.children.len() as u32)
+                .map(|i| self.children[*i as usize - 1].value())
+                .sum()
+        }
+    }
+}
+
+pub struct Solution08;
+impl Solution08 {
+    fn parse(input: ProblemInput) -> Vec<u32> {
+        input.string().split_whitespace().parsed().collect()
+    }
+}
+
+impl Solution for Solution08 {
+    fn solve_version01(&self, input: ProblemInput, _is_sample: bool) -> Option<ProblemResult> {
+        let licence = Self::parse(input);
+        let root = Node::from_licence(&mut licence.into_iter());
+        root.total_metadata().into_some()
+    }
+
+    fn solve_version02(&self, input: ProblemInput, _is_sample: bool) -> Option<ProblemResult> {
+        let licence = Self::parse(input);
+        let root = Node::from_licence(&mut licence.into_iter());
+        root.value().into_some()
+    }
+}
