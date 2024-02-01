f698273d6a82f30b53d76c444e490f6e541ec473
Marek Hummel <marek.hummel3@gmail.com>
2024-02-02 00:30:29 +0100

    Solved #03 in 2019

---
+++ 2019/rust/solutions/day03.rs
@@ -0,0 +1,81 @@
+use aoc_lib::cartesian::{Direction, Position};
+use aoc_lib::solution::Solution;
+use aoc_lib::types::{IntoSome, ProblemInput, ProblemResult};
+use itertools::Itertools;
+use std::collections::HashSet;
+
+type Path = Vec<(Direction, u32)>;
+
+pub struct Solution03;
+impl Solution03 {
+    fn parse(input: ProblemInput) -> (Path, Path) {
+        input
+            .lines()
+            .into_iter()
+            .map(|line| {
+                line.split(',')
+                    .map(|segment| {
+                        let (dir_str, length_str) = segment.split_at(1);
+                        let dir = match dir_str {
+                            "U" => Direction::North,
+                            "R" => Direction::East,
+                            "D" => Direction::South,
+                            "L" => Direction::West,
+                            _ => unreachable!(),
+                        };
+
+                        (dir, length_str.parse().unwrap())
+                    })
+                    .collect()
+            })
+            .collect_tuple()
+            .unwrap()
+    }
+
+    fn trace_path(path: Path) -> Vec<Position> {
+        path.into_iter()
+            .fold((Vec::new(), Position::zero()), |(mut trace, mut pos), (dir, length)| {
+                for _ in 0..length {
+                    pos = pos.advance_by(dir, 1);
+                    trace.push(pos);
+                }
+
+                (trace, pos)
+            })
+            .0
+    }
+}
+
+impl Solution for Solution03 {
+    fn solve_version01(&self, input: ProblemInput, _is_sample: bool) -> Option<ProblemResult> {
+        let (path1, path2) = Self::parse(input);
+        let trace1: HashSet<_> = HashSet::from_iter(Self::trace_path(path1));
+        let trace2: HashSet<_> = HashSet::from_iter(Self::trace_path(path2));
+
+        let intersections = trace1.intersection(&trace2);
+
+        intersections
+            .into_iter()
+            .map(|pos| pos.dist(&Position::zero()))
+            .min()
+            .unwrap()
+            .into_some()
+    }
+
+    fn solve_version02(&self, input: ProblemInput, _is_sample: bool) -> Option<ProblemResult> {
+        let (path1, path2) = Self::parse(input);
+        let trace1 = Self::trace_path(path1);
+        let trace2 = Self::trace_path(path2);
+
+        let trace_set1: HashSet<_> = HashSet::from_iter(trace1.iter());
+        let trace_set2: HashSet<_> = HashSet::from_iter(trace2.iter());
+        let intersections = trace_set1.intersection(&trace_set2);
+
+        let closest_intersection = intersections
+            .into_iter()
+            .map(|&pos| trace1.iter().position(|t| t == pos).unwrap() + trace2.iter().position(|t| t == pos).unwrap())
+            .min()
+            .unwrap();
+        (closest_intersection + 2).into_some()
+    }
+}
