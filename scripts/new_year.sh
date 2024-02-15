1cb0ef61b0f197966f08633fe2e22b6ebee4e27c
Marek Hummel <marek.hummel3@gmail.com>
2024-02-15 17:19:49 +0100

    Fixed new year script

--- scripts/new_year.sh
+++ scripts/new_year.sh
@@ -7,4 +7,4 @@ touch $1/rust/solutions/mod.rs
 cp scripts/templates/main_template.rs $1/rust/main.rs
 sed -i s/XXXX/$1/ $1/rust/main.rs
 
-sed -i "9 a|[[bin]]\nname = "main$1"\npath = "$1/rust/main.rs"\n" Cargo.toml
+sed -i "9 a [[bin]]\nname = \"main$1\"\npath = \"$1/rust/main.rs\"\n" Cargo.toml
