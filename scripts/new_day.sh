956132c05b631ba8a67dd3bce9c580d82d735485
Marek Hummel <marek.hummel3@gmail.com>
2024-02-15 17:17:28 +0100

    Updated scripts

--- scripts/new_day.sh
+++ scripts/new_day.sh
@@ -1,11 +1,16 @@
 year=$1
 day=$(printf "%02d" $2)
-cp scripts/day_template.rs $year/rust/solutions/day$day.rs
+cp scripts/templates/day_template.rs $year/rust/solutions/day$day.rs
 sed -i s:XX:$day: $year/rust/solutions/day$day.rs
 
 sed -i "$(($2+2)) a\use solutions::day$day;" $year/rust/main.rs 
 sed -i "$(($2*2+12)) a\        Box::new(day$day::Solution$day {})," $year/rust/main.rs 
-sed -i "$(($2-1)) a\pub mod day$day;" $year/rust/solutions/mod.rs
+
+if [ $2 -eq 1 ]; then 
+    echo "pub mod day01;" > $year/rust/solutions/mod.rs
+else 
+    sed -i "$(($2-1)) a\pub mod day$day;" $year/rust/solutions/mod.rs
+fi
 
 cookie=$(cat scripts/aoc_cookie.txt)
 curl "https://adventofcode.com/$year/day/$2/input" -H "cookie: session=$cookie" -o "$year/inputs/input$day.txt" 2>/dev/null
---
+++ scripts/new_year.sh
@@ -0,0 +1,10 @@
+mkdir $1
+mkdir $1/inputs
+mkdir $1/rust
+mkdir $1/rust/solutions
+
+touch $1/rust/solutions/mod.rs
+cp scripts/templates/main_template.rs $1/rust/main.rs
+sed -i s/XXXX/$1/ $1/rust/main.rs
+
+sed -i "9 a|[[bin]]\nname = "main$1"\npath = "$1/rust/main.rs"\n" Cargo.toml
