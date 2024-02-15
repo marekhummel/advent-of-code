47d78f603edfd160f3688088b6ead3321ed3132e
Marek Hummel <marek.hummel3@gmail.com>
2024-02-15 17:25:55 +0100

    Solved #01 in 2020

---
+++ 2020/rust/solutions/day01.rs
@@ -0,0 +1,37 @@
+use aoc_lib::iterator::ParsedExt;
+use aoc_lib::solution::Solution;
+use aoc_lib::types::{ProblemInput, ProblemResult, ToResult};
+use itertools::Itertools;
+
+pub struct Solution01;
+impl Solution01 {
+    fn parse(input: ProblemInput) -> Vec<u32> {
+        input.lines().into_iter().parsed().collect()
+    }
+}
+
+impl Solution for Solution01 {
+    fn solve_version01(&self, input: ProblemInput, _is_sample: bool) -> ProblemResult {
+        let entries = Self::parse(input);
+
+        entries
+            .into_iter()
+            .combinations(2)
+            .find(|pair| pair.iter().sum::<u32>() == 2020)
+            .map(|pair| pair.iter().product::<u32>())
+            .unwrap()
+            .to_result()
+    }
+
+    fn solve_version02(&self, input: ProblemInput, _is_sample: bool) -> ProblemResult {
+        let entries = Self::parse(input);
+
+        entries
+            .into_iter()
+            .combinations(3)
+            .find(|pair| pair.iter().sum::<u32>() == 2020)
+            .map(|pair| pair.iter().product::<u32>())
+            .unwrap()
+            .to_result()
+    }
+}
