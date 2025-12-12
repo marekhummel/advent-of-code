545b95b6602f277f6c207ec927b6d0351f1ef98c
Marek Hummel <marek.hummel3@gmail.com>
2025-12-12 23:49:50 +0100

    Solved 2025-12 in python (simple)

--- 2025/python/main.py
+++ 2025/python/main.py
@@ -1,10 +1,12 @@
 from lib.python import Runner, Solution
 
 from .solutions.day10 import Solution10
+from .solutions.day12 import Solution12
 
 if __name__ == "__main__":
     solutions: dict[int, type[Solution]] = {
         10: Solution10,
+        12: Solution12,
     }
 
     runner = Runner(2025, solutions=solutions)
